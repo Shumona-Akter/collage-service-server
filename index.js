e014838839c6c42ac2a8af9f167243093db36f18
Shumona-Akter <shumona.cse.bd@gmail.com>
2023-07-24 23:44:42 +0600

    spesific data create

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://collage-book-service:8JK9pR8FmaBweczK@cluster0.tjvax7z.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,6 +22,20 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const collageCollection = client.db("Collage-book-service").collection("Collage")
+
+    app.get("/collage",async (req, res)=>{
+      const cursor = collageCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.get('/collage/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await collageCollection.findOne(query);
+      res.send(result);
+    });
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -32,8 +46,8 @@ run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
+})
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
    
-  })
+})
